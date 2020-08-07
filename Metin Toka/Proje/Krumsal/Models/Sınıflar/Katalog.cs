e0afb868f3ba26c866ef81f5abae450e97dca3f9
furkanltun <51624672+furkanltun@users.noreply.github.com>
2020-08-07 09:37:20 +0300

    Güncel

---
+++ Metin Toka/Proje/Krumsal/Models/Sınıflar/Katalog.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Kurumsal.Models.Sınıflar
+{
+    [Table("Katalog")]
+    public class Katalog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Baslik { get; set; }
+        public string ResimURL { get; set; }
+    }
+}
